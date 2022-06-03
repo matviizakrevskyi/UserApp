e2ec0f15957e013fee29dabeb60a379f7bfa75f8
matviizakrevskyi <matvii.zakrevskyi@microsoft.wsei.edu.pl>
2022-06-04 01:37:05 +0200

    2 - registration checks

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,45 @@ namespace UserApp
 
         private void ButtonSignUpClick(object sender, RoutedEventArgs e)
         {
-            
+            string login = textBoxLogin.Text.Trim();
+            string password = passBox.Password.Trim();
+            string passwordRep = passBoxRep.Password.Trim();
+            string email = textBoxEmail.Text.Trim();  //.ToLower().Trim() - фильтры
+
+            if (login.Length < 5)   //---  проверка логина
+            {
+                textBoxLogin.ToolTip = "This field is entered incorrectly!";   //подсказка когда наводишь мышью
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (password.Length < 5)  //---  проверка пароля
+            {
+                passBox.ToolTip = "This field is entered incorrectly!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (password != passwordRep)  //---  проверка повтора пароля
+            {
+                passBoxRep.ToolTip = "Passwords do not match!";
+                passBoxRep.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))  //---  проверка емейла
+            {
+                textBoxEmail.ToolTip = "Passwords do not match!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else  //--- если проверка прошла успешно ---//
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBoxRep.ToolTip = "";
+                passBoxRep.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Nice!!!");
+            }
+
         }
     }
 }
